d85f80b2285696aa61e5918550db4ecdc7b1a082
thekhuong97 <thekhuong97@gmail.com>
2025-06-17 18:57:39 +0700

    edit task

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ const cancelModalBtn = $(".btn-cancel");
 const addTaskModal = $("#addTaskModal");
 const todoForm = $(".todo-app-form");
 const titleInput = $("#taskTitle");
+const todoList = $("#todoList");
+
+let editIndex = null;
 
 // Hàm mở form & focus
-function openForm() {
+function openFormModal() {
   addTaskModal.className = "modal-overlay show";
   setTimeout(() => {
     titleInput.focus();
@@ -18,12 +21,27 @@ function openForm() {
 
 // Hàm reset form + đóng modal
 function closeForm() {
-  todoForm.reset();
   addTaskModal.className = "modal-overlay";
+
+  const formTitle = addTaskModal.querySelector(".modal-title");
+  if (formTitle) {
+    formTitle.textContent = formTitle.dataset.original || formTitle.textContent;
+    delete formTitle.dataset.original;
+  }
+
+  const submitBtn = addTaskModal.querySelector(".btn-submit");
+  if (submitBtn) {
+    submitBtn.textContent = submitBtn.dataset.original || submitBtn.textContent;
+    delete submitBtn.dataset.original;
+  }
+
+  todoForm.reset();
+
+  editIndex = null;
 }
 
 // Hiển thị modal thêm mới + focus input
-addBtn.onclick = openForm;
+addBtn.onclick = openFormModal;
 
 // Xử lý đóng modal thêm mới
 closeModalBtn.onclick = closeForm;
@@ -36,11 +54,22 @@ todoForm.onsubmit = function (event) {
   event.preventDefault();
 
   // Lấy toàn bộ form data (dữ liệu từ các input, textarea,...)
-  const newTask = Object.fromEntries(new FormData(todoForm));
-  newTask.isCompleted = false; // Mặc định task chưa được hoàn thành
+  const formData = Object.fromEntries(new FormData(todoForm));
 
-  //   Thêm dữ liệu vào đầu mảng
-  todoTasks.unshift(newTask);
+  // Nếu có editIndex tức đang mở modal sửa
+  // Thực hiện logic sửa
+  if (editIndex) {
+    todoTasks[editIndex] = formData;
+  }
+  // Không editIndex, tức đang mở modal thêm mới
+  // Thực hiện logic thêm mới
+  else {
+    // Mặc định task chưa được hoàn thành
+    formData.isCompleted = false;
+
+    //   Thêm task vào đầu danh sách
+    todoTasks.unshift(formData);
+  }
 
   //Lưu toàn bộ danh sách task vào localStorage
   localStorage.setItem("todoTasks", JSON.stringify(todoTasks));
@@ -52,10 +81,42 @@ todoForm.onsubmit = function (event) {
   renderTasks(todoTasks);
 };
 
+todoList.onclick = function (event) {
+  const editBtn = event.target.closest(".edit-btn");
+
+  //Edit
+  if (editBtn) {
+    const taskIndex = editBtn.dataset.index;
+    const task = todoTasks[taskIndex];
+
+    editIndex = taskIndex;
+
+    for (const key in task) {
+      const value = task[key];
+      const input = $(`[name="${key}"]`);
+      if (input) {
+        input.value = value;
+      }
+    }
+
+    const formTitle = addTaskModal.querySelector(".modal-title");
+    if (formTitle) {
+      formTitle.dataset.original = formTitle.textContent;
+      formTitle.textContent = "Edit Task";
+    }
+
+    const submitBtn = addTaskModal.querySelector(".btn-submit");
+    if (submitBtn) {
+      submitBtn.dataset.original = submitBtn.textContent;
+      submitBtn.textContent = "Save Task";
+    }
+
+    openFormModal();
+  }
+};
+
 // Hàm render giao diện
 function renderTasks(tasks) {
-  const todoList = $("#todoList");
-
   if (!tasks.length) {
     todoList.innerHTML = "<p>Chưa có công việc nào.</p>";
     return;
@@ -63,7 +124,7 @@ function renderTasks(tasks) {
 
   const html = tasks
     .map(
-      (task) => `
+      (task, index) => `
             <div class="task-card ${task.color} ${
         task.isCompleted ? "completed" : ""
       }">
@@ -72,7 +133,7 @@ function renderTasks(tasks) {
             <button class="task-menu">
               <i class="fa-solid fa-ellipsis fa-icon"></i>
               <div class="dropdown-menu">
-                <div class="dropdown-item">
+                <div class="dropdown-item edit-btn" data-index="${index}">
                   <i class="fa-solid fa-pen-to-square fa-icon"></i>
                   Edit
                 </div>
